846a5cd8af8e80108c1e54dc67325aeda4d0d237
RRoroan <tjdals8788@gmail.com>
2025-02-18 19:31:45 +0900

    [Feat] Giving more details about the minigame
    Given more details about the MiniGame
    1. following the Player
    2. can adjust speed and follow range
    
    Around 50% done.

--- Assets/Script/Entity/MiniGameEnemy.cs
+++ Assets/Script/Entity/MiniGameEnemy.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class MiniGameEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SpriteRenderer enemyCharater;
+    [SerializeField] private float followRange = 15f;
+    [Range(1,5)] [SerializeField] private float speed = 1.5f;
+    private Transform target;
+    private bool playerTouch;
+    private Vector2 movementDirection = Vector2.zero;
+    private Vector2 MovementDirection { get { return movementDirection; } }
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        _rigidbody2D.velocity = movementDirection * speed;
     }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float distance = DistancetoTarget();
+        
+        if (distance > followRange)
+        {
+            movementDirection = Vector2.zero;
+            return;
+        }
+        Vector2 direction = DirectiontoTarget();
+        movementDirection = direction;
+    }
+
+    private float DistancetoTarget ()
+    {
+        return Vector3.Distance(transform.position, target.position);
+    }
+
+    private Vector2 DirectiontoTarget()
+    {
+        return (target.position - transform.position).normalized;
+    }
+
 }
