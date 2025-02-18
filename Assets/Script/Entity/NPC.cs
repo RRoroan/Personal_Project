21f3ec03f3cafa4365cad54d7b47960e87ca4c8c
RRoroan <tjdals8788@gmail.com>
2025-02-18 15:39:23 +0900

    [Feat] Add NPC dialogue and Next Scene

--- Assets/Script/Entity/NPC.cs
+++ Assets/Script/Entity/NPC.cs
@@ -9,6 +9,78 @@ public class NPC : MonoBehaviour
     [SerializeField] private Text dialogueText;
     public string[] dialogue;
     public float wordSpeed;
+    public int index;
+    public bool playerIsClosed;
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private GameObject HelpPanel;
 
-    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && playerIsClosed)
+        {
+            if (dialoguePanel.activeInHierarchy)
+            {
+                ResetText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                HelpPanel.SetActive(false);
+                StartCoroutine(Typing());
+            }
+        }
+        if(dialogueText.text == dialogue[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = " ";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            ResetText();
+        }
+    }
+
+    public void ResetText()
+    {
+        dialogueText.text = " ";
+        index = 0;
+        dialoguePanel.SetActive(false);
+        HelpPanel.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerIsClosed = true;
+            HelpPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerIsClosed = false;
+            ResetText();
+        }
+    }
 }
